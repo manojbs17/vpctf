388b68f2d71cac2840dc217741b905a692ea6bc9
manojbs17 <mmanoj21064@gmail.com>
2024-08-19 10:46:46 +0530

    public.tf

--- public.tf
+++ public.tf
@@ -9,14 +9,6 @@ resource "aws_subnet" "public_a" {
   cidr_block        = var.subnets.a
   availability_zone = "ap-south-1a"
 }
-
-resource "aws_nat_gateway" "nat-gw-2a-public" {
-  allocation_id = aws_eip.eip_a.id
-  subnet_id     = aws_subnet.public_a.id
-  tags      = merge(var.tags, {Name = "nat-gw-2a-public"})
-  depends_on = [aws_eip.eip_a]
-}
-
 resource "aws_route_table" "rt_public_a" {
   vpc_id = aws_vpc.amc-vpc.id
    tags   = merge(var.tags, {Name = "rt_public_a"})
